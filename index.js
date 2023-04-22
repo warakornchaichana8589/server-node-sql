481f1a0c6f568c1dde4582778641743dd27378f7
warakornchaichana8589 <119112758+warakornchaichana8589@users.noreply.github.com>
2023-04-22 15:41:12 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 const cors = require('cors');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
-const fileupload = require("express-fileupload");
 const app = express();
 
 const connection = mysql.createConnection(process.env.DATABASE_URL)
@@ -12,24 +11,10 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static("files"));
-app.use(fileupload())
 
 app.get('/',(req, res) =>{
     res.send("Hello World")
 })
-
-app.post("/upload", async (req, res) => {
-    const newpath = __dirname + "/image/";
-    const file = req.files.file;
-    const filename = file.name;
-   
-    file.mv(`${newpath}${filename}`, (err) => {
-      if (err) {
-        res.status(500).send({ message: "File upload failed", code: 200 });
-      }
-      res.status(200).send({ message: "File Uploaded", code: 200 });
-    });
-  });
 
 app.get('/project', (req, res) => {
     connection.query(
