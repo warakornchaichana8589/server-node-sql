0c9f2e6af5e71e7739b7dbfd9bb76886f2ad3622
warakornchaichana8589 <119112758+warakornchaichana8589@users.noreply.github.com>
2023-04-22 15:43:13 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+require('dotenv').config()
+const cors = require('cors');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const app = express();
+
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/',(req, res) =>{
+    res.send("Hello World")
+})
+
+app.get('/project', (req, res) => {
+    connection.query(
+        'SELECT * FROM project',
+       function(err, results, fields) {
+            res.send(results)
+        }
+    )
+})
+app.get('/admin', (req, res) => {
+    connection.query(
+        'SELECT * FROM admin',
+       function(err, results, fields) {
+            res.send(results)
+        }
+    )
+})
+app.post('/add/admin',(req, res) => {
+        connection.query(
+            'INSERT INTO `admin` (`username`, `password`) VALUES (?, ?)',
+            [req.body.username, req.body.password],
+                function (err, results) {
+                res.json(results);
+    }
+        )
+})
+app.post('/add/project',(req, res) => {
+    connection.query(
+        'INSERT INTO `project` (`name`, `description`, `project_image`) VALUES (?, ?, ?)',
+        [req.body.name, req.body.description, req.body.project_image],
+            function (err, results) {
+            res.json(results);
+}
+    )
+})
+
+
+app.listen(process.env.PORT || 3000)
